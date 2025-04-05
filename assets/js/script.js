c6296859df085593cae403d2ce67f9e234bdc9a8
Andrey Belyakov <rayrah@live.com>
2025-04-05 22:34:55 +0100

    Add scripts to enable navigation-link hightlighting on scroll.

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ const menuLinks = navMenu.querySelectorAll(".navigation-link");
 const whiteLogo = myNav.querySelector("#navigation-bar-logo-white");
 const blackLogo = myNav.querySelector("#navigation-bar-logo-black");
 const logoContainer = myNav.querySelector("#logo");
-// const activeNavLink = myNav.querySelector(".active");
+const activeNavLink = myNav.querySelector(".active");
 let isMenuOpen = false;
 
 /* Navigation Bar */
@@ -43,4 +43,39 @@ function setTransparentNavbar() {
   whiteLogo.classList.add("transparent");
   blackLogo.classList.remove("transparent");
   Array.from(menuLinks).forEach(menuLink => menuLink.classList.add("black"));
+}
+
+/* Scroll Tracker */
+
+// Get all sections that have an ID defined.
+const sections = document.querySelectorAll("section[id]");
+
+// Add an event listener listening for scroll.
+window.addEventListener("scroll", navHighlighter);
+
+function navHighlighter() {
+
+  // Get current scroll position.
+  let scrollY = window.pageYOffset;
+
+  // Now we loop through sections to get height, top and ID values for each
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 85;
+    sectionId = current.getAttribute("id");
+
+    /*
+    - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it.
+    - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as a selector.
+    */
+
+    if (
+      scrollY > sectionTop &&
+      scrollY <= sectionTop + sectionHeight
+    ){
+      document.querySelector("#navigation-menu a[href*=" + sectionId + "]").classList.add("active");
+    } else {
+      document.querySelector("#navigation-menu a[href*=" + sectionId + "]").classList.remove("active");
+    }
+  });
 }
